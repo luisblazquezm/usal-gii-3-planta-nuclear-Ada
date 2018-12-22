856fbbe7a8aab1ad394f5a654ff2807f6fd2be98
luisblazquezm <luisblazquezm@usal.es>
2018-12-22 18:55:47 +0100

    Poniendolo a punto 22/12
    - No quites ningún comentario que los he puesto yo todos para guiarnos en lo que ocurre en el programa y lo que va sucediendo.
    - El codigo comentado es del anterior que tenian ellos pero es bueno dejarlo para comparar con lo que tenemos que hacer nosotros. Lo mismo, que nos sirva un poco de guía
    - Basicamente no es muy complicado nuestro ejercicio pero hay que cambiar alguna cosilla (aunque este trabajo no debería durarnos demasiado)

--- PracticaADA/src/main.adb
+++ PracticaADA/src/main.adb
@@ -12,179 +12,160 @@ with Ada.Numerics.Discrete_Random;
 
 procedure Main is
 
+   -- Numeros aleatorios
    subtype ConsumptionVariance_t is Integer range -3..3;
    package RNG is new Ada.Numerics.Discrete_Random(ConsumptionVariance_t);
 
    subtype PowerConsumption_t is Integer range 15..90;
    subtype PlantOutput_t is Integer range 0..30;
 
-   protected type City_t is
-      procedure varyConsumption;
-      function getConsumption return PowerConsumption_t;
-   private
-      consumption:PowerConsumption_t:=35;
-   end City_t;
-
-   protected body City_t is
-
-      procedure varyConsumption is
-         variance:ConsumptionVariance_t;
-         seed:RNG.Generator;
-      begin
-         RNG.Reset(seed);
-         variance := RNG.Random(seed);
-         Consumption := Consumption + variance;
-      exception
-            when Constraint_Error => null;
-      end;
-
-      function getConsumption return PowerConsumption_t is
-      begin
-         return consumption;
-      end getConsumption;
-
-   end City_t;
-
-   city:City_t;
-
+   -- 3 reactores nucleares
    plant1: aliased PowerPlant_t;
    plant2: aliased PowerPlant_t;
    plant3: aliased PowerPlant_t;
 
-   task type tskVaryPowerConsumption;
-   task body tskVaryPowerConsumption is
-
+   -- Tarea de variación de la temperatura en el reactor
+   task type tskVaryTemperature;
+   task body tskVaryTemperature is
       tNextRelease: Time;
       tiReleaseInterval:constant Time_Span:=Milliseconds(1000);
-
-
    begin
 
       tNextRelease := Clock + tiReleaseInterval;
       loop
-         city.varyConsumption;
+         --#city.varyConsumption;
          delay until tNextRelease;
          tNextRelease := tNextRelease + tiReleaseInterval;
       end loop;
 
-   end tskVaryPowerConsumption;
+   end tskVaryTemperature;
 
-   PowerVariance: tskVaryPowerConsumption;
+   -- Objeto tarea de variación de la temperatura
+   TempVariance: tskVaryTemperature;
 
 
+   -- Procedimiento: recoge la temperatura total de los tres reactores
+   --                en las variables out1, out2 y out3
+   procedure getTotalTemperature(temp1:out Temperature_t; temp2:out Temperature_t; temp3:out Temperature_t) is
 
-   procedure getTotalOutput(out1:out Output_t; out2:out Output_t; out3:out Output_t) is
       task type p1;
       task body p1 is
       begin
-         out1 := plant1.getOutput;
+         temp1 := plant1.getOutput;
       end p1;
+
       task type p2;
       task body p2 is
       begin
-         out2 := plant1.getOutput;
+         temp2 := plant2.getOutput;
       end p2;
+
       task type p3;
       task body p3 is
       begin
-         out3 := plant3.getOutput;
+         temp3 := plant3.getOutput;
       end p3;
 
       taskp1:p1;
       taskp2:p2;
       taskp3:p3;
+
    begin
-   	   null;
-   end getTotalOutput;
+      null;
+   end getTotalTemperature;
 
 
+   -- Tarea Principal que realizará el monitor o tarea coordinadora cada 2 segundos
    task type tskMonitorConsumption;
    task body tskMonitorConsumption is
       tNextRelease: Time;
       tiReleaseInterval:constant Time_Span:=Milliseconds(200);
 
+      -- Temperatura de los 3 reactores
+      temp1:Temperature_t;
+      temp2:Temperature_t;
+      temp3:Temperature_t;
 
-
-      cityConsumption:PowerConsumption_t;
-
-
-
-      out1:Output_t;
-      out2:Output_t;
-      out3:Output_t;
-
-      output:Integer;
+      -- Temperatura del nucleo del reactor (la suma de la temp. de los tres reactores)
+      nucleusTemp:Integer;
       diff:Integer;
 
       type plant_ptr is access all PowerPlant_t;
 
+      -- 3 reactores nucleares / 1 tarea por reactor nuclear
       first:plant_ptr:=plant1'access;
       second:plant_ptr:=plant2'Access;
       third:plant_ptr:=plant3'Access;
 
    begin
+      -- Se les asigna un id a cada reactor. Sirve sobre todo para identificarlos.
       plant1.setID(1);
       plant2.setID(2);
       plant3.setID(3);
+
       tNextRelease := Clock + tiReleaseInterval;
+
       loop
-         --delay 6.0;
-         getTotalOutput(out1, out2, out3);
+
+         getTotalTemperature(temp1, temp2, temp3);
+
+         --Delay de 1 segundo para simular la lectura de la temperatura
          delay 0.1;
-         cityConsumption := City.getConsumption;
-         output := out1 + out2 + out3;
-         if (out3 >= out1 and then out3 >= out2) then
-            first := plant3'access;
-            if (out2 > out1) then
-               third := plant1'Access;
-            else
-               second := plant1'Access;
-               third := plant2'Access;
-            end if;
-         elsif (out2 > out3 and then out2 > out1) then
-            first := plant2'access;
-            if (out3 >= out1) then
-               second := plant3'Access;
-               third := plant1'Access;
-            else
-               second := plant1'Access;
-            end if;
-         elsif (out3 >= out2) then
-            second:=plant3'Access;
-            third:=plant2'Access;
+         nucleusTemp := temp1 + temp2 + temp3;
+         --if (temp3 >= temp1 and then temp3 >= temp2) then
+            --first := plant3'access;
+            --if (temp2 > temp1) then
+               --third := plant1'Access;
+            --else
+               --second := plant1'Access;
+               --third := plant2'Access;
+            --end if;
+         --elsif (temp2 > out3 and then temp2 > temp1) then
+            --first:= plant2'access;
+            --if (temp3 >= temp1) then
+               --second := plant3'Access;
+               --third:= plant1'Access;
+            --else
+               --second:= plant1'Access;
+            --end if;
+         --elsif (temp3 >= temp2) then
+            --second:=plant3'Access;
+            --third:=plant2'Access;
          end if;
 
 
+         -- CASO 1: temperatura es superior a (1500ºC)
+      	 if (Float(nucleusTemp) >= 1500 and then Float(nucleusTemp) <= 1750) then
 
-      	 if ((Float(cityConsumption) - Float(output))/Float(cityConsumption) > 0.05) then
-            --Put_Line("PELIGRO BAJADA consumo:"&Integer'Image(cityConsumption)&" producción:"&Integer'Image(output));
-            diff := cityConsumption-output;
-            third.setOperation(1);
-            if (diff >= 2) then second.setOperation(1); else second.setOperation(0); end if;
-            if (diff >= 3) then first.setOperation(1); else first.setOperation(0); end if;
-            Put_Line("PELIGRO BAJADA consumo:"&Integer'Image(cityConsumption)&" producción:"&Integer'Image(output));
+            --diff := cityConsumption-nucleusTemp;
+            --third--.setOperation(1);
+            --if (diff >= 2) then second.setOperation(1); else second.setOperation(0); end if;
+            --if (diff >= 3) then first.setOperation(1); else first.setOperation(0); end if;
+            --Put_Line("PELIGRO BAJADA consumo:"&Integer'Image(cityConsumption)&" producción:"&Integer'Image(nucleusTemp));
 
-      	 elsif ((Float(cityConsumption) - Float(output))/Float(cityConsumption) < -0.05) then
-            --Put_Line("PELIGRO SOBRECARGA consumo:"&Integer'Image(cityConsumption)&" producción:"&Integer'Image(output)&"= "&Integer'Image(out1)&" + "&Integer'Image(out2)&" + "&Integer'Image(out3));
-            diff := output - cityConsumption;
-            first.setOperation(-1);
-            if (diff >= 2) then second.setOperation(-1); else second.setOperation(0); end if;
-            if (diff >= 3) then third.setOperation(-1); else third.setOperation(0); end if;
-            Put_Line("PELIGRO SOBRECARGA consumo:"&Integer'Image(cityConsumption)&" producción:"&Integer'Image(output));
+         -- CASO 2: temperatura es superior a 1750ºC
+      	 elsif (Float(nucleusTemp) > 1750) then
 
+            --diff := nucleusTemp - cityConsumption;
+            --first--.setOperation(-1);
+            --if (diff >= 2) then second.setOperation(-1); else second.setOperation(0); end if;
+            --if (diff >= 3) then third.setOperation(-1); else third.setOperation(0); end if;
+            --Put_Line("PELIGRO SOBRECARGA consumo:"&Integer'Image(cityConsumption)&" producción:"&Integer'Image(nucleusTemp));
+
+         -- CASO 3: temperatura inferior a 1500ºC
          else
-            --Put_Line("Estable consumo:"&Integer'Image(cityConsumption)&" producción:"&Integer'Image(output)&"= "&Integer'Image(out1)&" + "&Integer'Image(out2)&" + "&Integer'Image(out3));
-            if (out1 = out2 and then out2 = out3) then
-               first.setOperation(0);
-               second.setOperation(0);
-               third.setOperation(0);
-            else
-               first.setOperation(0);
-               second.setOperation(0);
-               third.setOperation(0);
-            end if;
 
-            Put_Line("Estable consumo:"&Integer'Image(cityConsumption)&" producción:"&Integer'Image(output));
+            --if (temp1 = temp2 and then temp2 = temp3) then
+               --first.setOperation(0);
+               --second--.setOperation(0);
+               --third.setOperation(0);
+            --else
+               --first.setOperation(0);
+               --second.setOperation(0);
+               --third--.setOperation(0);
+            --end if;
+
+            --Put_Line("Estable consumo:"&Integer'Image(cityConsumption)&" producción:"&Integer'Image(nucleusTemp));
 
          end if;
          delay until tNextRelease;
@@ -195,8 +176,7 @@ procedure Main is
 
    PowerMonitoring:tskMonitorConsumption;
 
-
 begin
-null;
+   null;
 end Main;
 
